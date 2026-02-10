6b0ea8c12f4f231d33756b0cfbf8e679f0f58e6e
Boishakhi G <bgmukta11@gmail.com>
2026-02-10 23:52:42 +0100

    data filtered by category in homelayout

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const SocialLogin = () => {
   return (
     <div>
       <h1 className="font-bold">Login With</h1>
       <div className="space-y-2 mt-3">
-        <button className="btn border-blue-400 w-full">
-          Login with Google
+        <button className="btn button-outline btn-primary w-full h-auto py-3 flex items-center justify-center gap-2">
+          <FcGoogle className="text-2xl shrink-0" />
+          <span className="text-sm sm:text-base">Login with Google</span>
         </button>
-        <button className="btn border-gray-600 w-full">
-          Login with Github
+        <button className="btn button-outline btn-secondary w-full h-auto py-3 flex items-center justify-center gap-2">
+          <FaGithub className="text-2xl shrink-0" />
+          <span className="text-sm sm:text-base">Login with Github</span>
         </button>
       </div>
     </div>
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router";
 import { useParams } from "react-router";
+import NewsCard from "../components/NewsCard";
 
 const Category = () => {
-  const { id } = useParams();
+  const { id } = useParams(); //string
+  const data = useLoaderData();
+
+  const [categoryNews, setCategoryNews] = useState([]);
+
+  useEffect(() => {
+    if (id == "0") {
+      setCategoryNews(data);
+    } else if (id == "1") {
+      const breakingNews = data.filter(
+        (news) => news.others.is_today_pick == true,
+      );
+      setCategoryNews(breakingNews);
+    } else {
+      const filterData = data.filter((news) => news.category_id == id);
+      setCategoryNews(filterData);
+    }
+  }, [id, data]);
 
   return (
     <div>
-      <h1>CategoryNews ({id})</h1>
+      <h1 className="font-semibold">
+        Total <span className="text-secondary">{categoryNews.length}</span> data
+        found
+      </h1>
+      <div className="grid grid-cols-1 gap-3 mt-4">
+        {categoryNews.map((news) => (
+          <NewsCard key={news.id} news={news}></NewsCard>
+        ))}
+      </div>
     </div>
   );
 };
